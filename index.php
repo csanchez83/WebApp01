bce51f3ae473a986c3a526ef0b5eaf3246554221
csanchez83 <csanchez83@hotmail.com>
2024-10-08 18:03:40 -0600

    Estructura

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?
-    echo "Hola Mundo Carlos Alberto Sanchez";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Primera Web App en Azure</title>
+</head>
+<body>
+    <?
+        echo "<br><h5><center>Mi Primera Web App en Azure</center></h5>";
+        echo "<br><hr>";
+        echo "Este es un Mensaje en PHP";
+        echo "Hola Mundo Carlos Alberto Sanchez";
     ?>
+</body>
+</html>
+
